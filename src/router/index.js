17b59e662f0df93d82e56ce7a2e257eb3e78cf3c
daviddomene <daviddomene97@gmail.com>
2025-01-27 21:59:29 +0100

    .

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import Game from '../views/GameView.vue'; // Tu componente de la vista 'Game'
 const routes = [
-  {
-    path: '/home',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-  ,
   {
     path: '/', // Parámetros de ancho y alto en la URL
     name: 'Game',
